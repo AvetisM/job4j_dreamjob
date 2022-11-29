0a16952314660c58af88bfdfa2f988f2107769d8
AMkhitarants <avetis.mkhitaryants@gmail.com>
2022-11-29 17:32:23 +0300

    8. HTML form. Создание кандидата. [#504845 #365747]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -20,7 +20,7 @@ public class CandidateController {
     }
 
     @GetMapping("/formAddCandidate")
-    public String addPost(Model model) {
+    public String AddCandidate(Model model) {
         model.addAttribute("candidate", new Candidate(0, "Заполните название", "Заполните описание", LocalDateTime.now()));
         return "addCandidate";
     }
