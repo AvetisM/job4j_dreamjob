010f25337836ebda6b1d600a19b9ed9ac76afacd
AMkhitarants <avetis.mkhitaryants@gmail.com>
2022-12-01 09:48:00 +0300

    5. Список кандидатов [#504842 #365684]

--- src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
@@ -1,4 +1,4 @@
-package ru.job4j.controller;
+package ru.job4j.dreamjob.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
