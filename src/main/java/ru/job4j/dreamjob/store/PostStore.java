cc19fabed76ecb4cc319d5ca51edf0107c8f712b
AMkhitarants <avetis.mkhitaryants@gmail.com>
2022-12-02 23:04:06 +0300

    3. PostController.createPost. Создание вакансии. [#504849 #366499]

--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PostStore {
 
@@ -13,10 +14,15 @@ public class PostStore {
 
     private  final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
+    private final AtomicInteger currentId = new AtomicInteger(0);
+
     private PostStore() {
-        posts.put(1, new Post(1, "Junior Java Job", "Требуемый опыт - не требуется", LocalDateTime.now()));
-        posts.put(2, new Post(2, "Middle Java Job", "Требуемый опыт - меньше года", LocalDateTime.now()));
-        posts.put(3, new Post(3, "Senior Java Job", "Требуемый опыт - год и более", LocalDateTime.now()));
+        Post post1 = new Post(currentId.incrementAndGet(), "Junior Java Job", "Требуемый опыт - не требуется", LocalDateTime.now());
+        posts.put(post1.getId(), post1);
+        Post post2 = new Post(currentId.incrementAndGet(), "Middle Java Job", "Требуемый опыт - меньше года", LocalDateTime.now());
+        posts.put(post2.getId(), post2);
+        Post post3 = new Post(currentId.incrementAndGet(), "Senior Java Job", "Требуемый опыт - год и более", LocalDateTime.now());
+        posts.put(post3.getId(), post3);
     }
 
     public static PostStore instOf() {
@@ -27,15 +33,16 @@ public class PostStore {
         return posts.values();
     }
 
-    public void add (Post post) {
+    public void add(Post post) {
+        post.setId(currentId.incrementAndGet());
         posts.put(post.getId(), post);
     }
 
-    public Post findById (int id) {
+    public Post findById(int id) {
         return posts.get(id);
     }
 
-    public void update (Post post) {
+    public void update(Post post) {
         posts.replace(post.getId(), post);
     }
 }
